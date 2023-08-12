2a292f326697726918ba58da4379586395a31ef5
Mete Sayan <metesayan99@gmail.com>
2023-08-12 18:31:32 +0300

    feat: Tray window created & main window updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import MainApp from './apps/Main';
+import TrayApp from './apps/Tray';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <Switch >
+        <Route path='/tray' component={TrayApp} />
+        <Route path='/' component={MainApp} />
+      </Switch>
+    </HashRouter>
   );
 }
 
